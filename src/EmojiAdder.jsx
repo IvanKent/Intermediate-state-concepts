6b149823248b9cdf2ac6b09bc17decca6a7ecefe
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-01-18 10:40:14 +0800

    added id

--- src/EmojiAdder.jsx
+++ src/EmojiAdder.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react"
-
+import { v4 as uuidv4 } from 'uuid';
 export default function EmojiAdder({emojis}){
     const randomIndex = Math.floor(Math.random() * emojis.length)
-    const [emoji, setEmoji] = useState([emojis[randomIndex]])
+    const [emoji, setEmoji] = useState([{id: uuidv4(), emoji: emojis[randomIndex]}])
 
     function addEmoji() {
         const randomIndex = Math.floor(Math.random() * emojis.length)
         setEmoji(oldEmojis => {
-            return [...oldEmojis, emojis[randomIndex]]
+            return [...oldEmojis, {id: uuidv4(), emoji: emojis[randomIndex]}]
         })
     }
     return (
         <div>
             {emoji.map((e) => (
-                <span>{e}</span>
+                <span key={e.id} style={{fontSize: '3rem'}}>{e.emoji}</span>
             ))}
             <br />
             <button onClick={addEmoji}>add Emoji</button>
